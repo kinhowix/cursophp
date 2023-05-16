e0e009a81b32661be9a5ab8b0abea99b7161be6c
kinhowix <90629589+kinhowix@users.noreply.github.com>
2023-05-16 08:30:30 -0300

    Create conversao.php

---
+++ conversao.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <link rel="shortcut icon" href="favicon.ico" type="image/x-icon">
+    <title>Document</title>
+</head>
+<body>
+    <main>
+        <h1>Conversor de Moedas</h1>
+        <?php 
+   //cotação vinda do banco central<?php 
+    $inicio = date("m-d-Y" , strtotime("-7 days"));
+
+    $fim =date("m-d-Y");
+
+    $url='https://olinda.bcb.gov.br/olinda/servico/PTAX/versao/v1/odata/CotacaoDolarPeriodo(dataInicial=@dataInicial,dataFinalCotacao=@dataFinalCotacao)?@dataInicial=\''.$inicio.'\'&@dataFinalCotacao=\''.$fim.'\'&$top=1&$orderby=dataHoraCotacao%20desc&$format=json&$select=cotacaoCompra,dataHoraCotacao';
+
+    $dados=json_decode(file_get_contents($url), true);
+
+       $cotacao =$dados["value"][0]["cotacaoCompra"];
+      
+
+        $real = $_REQUEST["din"] ?? 0;
+        $dolar = $real / $cotacao;
+               
+
+        $padrao = numfmt_create("pt_BR", NumberFormatter::CURRENCY);
+
+        echo  "<p>Seus  ".numfmt_format_currency($padrao , $real , "BRL"). " equivalem
+        a <strong>".numfmt_format_currency($padrao , $dolar , "USD")."</strong></p>";
+
+        echo "<p> Cotação atualizada 1 dolar custa  : <strong>R$".number_format($cotacao ,2). "</strong></p></br>";     
+        
+        
+        
+        ?>
+             <p>Confira abaixo as informações no site do Banco Central</p>
+             <a href="https://www.bcb.gov.br">https://www.bcb.gov.br</a>
+        <button onclick="javascript:history.go(-1)">Voltar</button>
+
+            </main>
+            
+    
+</body>
+</html>
